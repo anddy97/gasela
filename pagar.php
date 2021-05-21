5aca6749d7e2e937bedbc6f82ac858aac88a4ba7
anddy97 <roqueanddy@gmail.com>
2021-05-20 23:10:25 -0300

    actualizacion numero 2

--- pagar.php
+++ pagar.php
@@ -2,7 +2,7 @@
 // SDK de Mercado Pago
 require __DIR__ .  '/vendor/autoload.php';
 // Agrega credenciales
-MercadoPago\SDK::setAccessToken('TEST-1384107962268824-051411-349f502cc81c16abda9b8722a775704f-260458337');
+MercadoPago\SDK::setAccessToken('APP_USR-3760857291646471-051907-b7216391184114d032ae5494e198afd9-761790929');
 
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
@@ -11,7 +11,7 @@ $preference = new MercadoPago\Preference();
 $item = new MercadoPago\Item();
 $item->title = 'Mi producto';
 $item->quantity = 1;
-$item->unit_price = 1.00;
+$item->unit_price = 50.00;
 $preference->items = array($item);
 $preference->save();
 ?>
@@ -34,19 +34,17 @@ $preference->save();
 <script src="https://sdk.mercadopago.com/js/v2"></script>
 
 
+<!-- curl -X POST -H"Content-Type: application/json" "https://api.mercadopago.com/users/test_user?access_token=TEST-1384107962268824-051411-349f502cc81c16abda9b8722a775704f-260458337" -d "{'site_id':'MLA'}"
 
-curl -X POST \
--H "Content-Type: application/json" \
--H 'Authorization: Bearer PROD_ACCESS_TOKEN' \
-"https://api.mercadopago.com/users/test_user" \
--d '{"site_id":"MLA"}'
+comprador de prueba={"id":761791128,"nickname":"TT665717","password":"qatest5487","site_status":"active","email":"test_user_97708682@testuser.com"}
 
-curl -X POST -H "Content-Type: application/json" -H 'Authorization:TEST-1384107962268824-051411-349f502cc81c16abda9b8722a775704f-260458337' "https://api.mercadopago.com/users/test_user"-d "{'site_id':'MLA'}"
+
+vendedor prueba= {"id":761790929,"nickname":"TEST1LRFOVAP","password":"qatest4203","site_status":"active","email":"test_user_76954730@testuser.com"} -->
 
           
 <script>
 // Agrega credenciales de SDK
-  const mp = new MercadoPago('TEST-4967bc3f-fc56-453f-9394-79e4449fcae1', {
+  const mp = new MercadoPago('APP_USR-4b9c4f87-0da2-4e6c-9273-12597ac15da0', {
         locale: 'es-AR'
   });
 
